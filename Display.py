4c2616a32816e8d2c7e1a1c8f93ce00a91fc94f4
SpaceTW <spacetw@att.net>
2015-10-20 16:58:14 -0500

    Fixed Errors in both classes

--- Display.py
+++ Display.py
@@ -2,35 +2,43 @@
 import Image
 import ImageDraw
 import time
+import random
 from rgbmatrix import Adafruit_RGBmatrix
-import Pattern
+from Pattern import Pattern
+from Pattern import SMB3
+from Pattern import SMB4
 
 # Rows and chain length are both required parameters:
-matrix = Adafruit_RGBmatrix(32, 1)
+#matrix = Adafruit_RGBmatrix(32, 1)
 
 class Display:
 	
 	def __init__(self):
-		#updateTimer=0
-		patternTimer=0
-		currentPattern=SMB3()
+		#self.updateTimer=0
+		self.matrix=Adafruit_RGBmatrix(32, 1)
+		self.patternTimer=0
+		self.currentPattern=SMB3()
 	
 	def start(self):
-		while(True)
+		while(True):
 			next=True
 			while(next):
-				next=not (update())
-				time.sleep(currentPattern.getTimestep)
+				next=not (self.update())
+				time.sleep(self.currentPattern.getTimeStep())
 			self.nextPattern()
 	
 	def drawPixels(self, image):
-		matrix.setImage(image, 0,0)
+		self.matrix.SetImage(image.im.id, 0,0)
 	
 	def update(self):
-		drawPixels()
+		self.drawPixels(self.currentPattern.getPixels())
+		return self.currentPattern.tick()
 	
-	def nextPattern():
-		currentPattern=SMB3()
+	def nextPattern(self):
+		if random.random()<.5 :
+			self.currentPattern=SMB3()
+		else :
+			self.currentPattern=SMB4()
 	
 	
 disp=Display()
--- Pattern.py
+++ Pattern.py
@@ -1,41 +1,52 @@
 
 import Image
 import ImageDraw
-import time
-from rgbmatrix import Adafruit_RGBmatrix
 
-# Rows and chain length are both required parameters:
-matrix = Adafruit_RGBmatrix(32, 1)
-
-class Patttern:
+class Pattern:
 	
 	def __init__(self, maxStates):
-		currentState=0
+		self.currentState=0
 		self.maxStates=maxStates
-		timeStep=1.0 #ms
+		self.timeStep=1.0 #ms
 	
 	def tick(self):
-		currentState+=1
-		return currentState>=maxStates
+		self.currentState+=1
+		return self.currentState>=self.maxStates
 	
 	def getPixels(self):
 		return
 		
 	def getTimeStep(self):
-		return timeStep
+		return self.timeStep
 		
 class SMB3(Pattern):
-	
-	
-	def __init__(self)
-		super().__init__(55)
-		timeStep=.5
-		image=Image.open("SMB3gif/SMB3-"+str(currentState)+".gif")
+	def __init__(self):
+		Pattern.__init__(self, 55)
+		self.timeStep=.09
+		self.image=Image.open("SMB3gif/SMB3-"+str(self.currentState)+".gif")
+		self.image.load()
 	
 	def tick(self):
-		currentState+=1
-		image=Image.open("SMB3gif/SMB3-"+str(currentState)+".gif")
-		return currentState>=maxStates
+		self.currentState+=1
+		self.image=Image.open("SMB3gif/SMB3-"+str(self.currentState)+".gif")
+		self.image.load()
+		return self.currentState>=self.maxStates
 	
 	def getPixels(self):
-		return image
+		return self.image
+		
+class SMB4(Pattern):
+	def __init__(self):
+		Pattern.__init__(self, 116)
+		self.timeStep=.09
+		self.image=Image.open("SMB4gif/SMB4-"+str(self.currentState)+".gif")
+		self.image.load()
+	
+	def tick(self):
+		self.currentState+=1
+		self.image=Image.open("SMB4gif/SMB4-"+str(self.currentState)+".gif")
+		self.image.load()
+		return self.currentState>=self.maxStates
+	
+	def getPixels(self):
+		return self.image
