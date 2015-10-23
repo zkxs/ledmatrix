2adab69333b005c6b7f72be22dcf606217f06aae
Michael Ripley <zkxs00@gmail.com>
2015-10-23 01:19:09 +0000

    Added Will's changes to Soundtest.py

--- SoundTest.py
+++ SoundTest.py
@@ -2,12 +2,20 @@
 
 import alsaaudio as aa
 import audioop
-from time import sleep
+import time
+import Image
+import ImageDraw
+import random
+from collections import deque
 
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-
+from rgbmatrix import Adafruit_RGBmatrix
+# Bitmap example w/graphics prims
+image = Image.new("RGB", (32, 32)) # Can be larger than matrix if wanted!!
+draw  = ImageDraw.Draw(image)    # Declare Draw instance before prims
+pastData=deque()
+for i in range(23):
+	pastData.append( (0, 0, 0) )
+matrix = Adafruit_RGBmatrix(32, 1)
 
 # Set up audio
 data_in = aa.PCM(aa.PCM_CAPTURE, aa.PCM_NONBLOCK)
@@ -18,16 +26,26 @@ data_in.setformat(aa.PCM_FORMAT_S16_LE)
 data_in.setperiodsize(256)
 
 while True:
-   # Read data from device
-   l,data = data_in.read()
-   if l:
-      # catch frame error
-      try:
-         max_vol=audioop.max(data,2)
-         scaled_vol = max_vol//4680      
-         print(scaled_vol)
-            
-            
-      except audioop.error, e:
-         if e.message !="not a whole number of frames":
-            raise e
+	# Read data from device
+	l,data = data_in.read()
+	if l:
+		# catch frame error
+		try:
+			max_vol=audioop.max(data,2)
+			scaled_vol = max_vol//4680      
+			print(max_vol)
+			pastData.popleft()
+			pastData.append(( max_vol//256, max_vol%256, 0 ))
+			for r in range(23, 0, -1):
+				color=pastData.popleft()
+				draw.ellipse((16-r, 16-r, 16+r, 16+r), outline=color)
+				pastData.append(color)
+				
+			matrix.SetImage(image.im.id, 0, 0)
+			time.sleep(0.025)
+
+
+
+		except audioop.error, e:
+			if e.message !="not a whole number of frames":
+				raise e
