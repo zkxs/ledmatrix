2850ce5fa3bcd875654c4c04215da00e12527466
Michael Ripley <zkxs00@gmail.com>
2015-10-23 17:47:18 +0000

    Patterns are now displayed indefinitely
    They are set using the -p or --pattern command line parameter

--- Display.py
+++ Display.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python
 
 import Image
 import ImageDraw
 import time
 import random
+import sys
+import getopt
 from rgbmatrix import Adafruit_RGBmatrix
 from Pattern import Pattern
 from Pattern import SMB3
@@ -18,19 +21,53 @@ class Display:
 		#self.updateTimer=0
 		self.matrix=Adafruit_RGBmatrix(32, 1)
 		self.patternTimer=0
-		self.currentPattern=TULogo()
+		self.nextPattern=None
+		
+		patternString = ''
+		helpString = 'Display.py -p <pattern>'
+
+		try:
+			opts, args = getopt.getopt(sys.argv[1:], "hp:", ["pattern"])
+		except getopt.GetoptError:
+			print helpString
+			sys.exit(2)
+
+		for opt, arg in opts:
+			if opt == '-h':
+				print helpString
+				sys.exit()
+			elif opt in ("-p", "--pattern"):
+				patternString = arg.upper()
+		
+		if patternString == "SMB3":
+			self.nextPattern = lambda: SMB3()
+			print "selected SMB3 pattern"
+		elif patternString == "SMB4":
+			self.nextPattern = lambda: SMB4()
+			print "selected SMB4 pattern"
+		elif patternString == "TULOGO":
+			self.nextPattern = lambda: TULogo()
+			print "selected TULOGO pattern"
+		elif patternString == "":
+			print helpString
+			sys.exit(2)
+		else:
+			print "Invalid pattern, options are: SMB3, SMB4, TULogo"
+			sys.exit(3)
 	
 	def start(self):
 		try:
 			while(True):
 				next=True
+				self.currentPattern = self.nextPattern()
 				while(next):
 					next=not (self.update())
 					time.sleep(self.currentPattern.getTimeStep())
-				self.nextPattern()
+				
 		except KeyboardInterrupt:
-			pass
-		self.matrix.Clear()
+			self.matrix.Clear()
+			sys.exit()
+		
 	
 	def drawPixels(self, image):
 		self.matrix.SetImage(image.im.id, 0,0)
@@ -38,16 +75,6 @@ class Display:
 	def update(self):
 		self.drawPixels(self.currentPattern.getPixels())
 		return self.currentPattern.tick()
-	
-	def nextPattern(self):
-		rand=random.random()
-		if rand<.33 :
-			self.currentPattern=SMB3()
-		elif rand<.66 :
-			self.currentPattern=SMB4()
-		else:
-			self.currentPattern=TULogo()
-	
-	
+
 disp=Display()
 disp.start()
