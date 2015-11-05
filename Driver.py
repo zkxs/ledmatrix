19eb2496bbf561106ddf3b4a843db4cab92f1519
Will Nichols <spacetw@att.net>
2015-11-05 20:26:55 +0000

    Added audioProcessor

--- Driver.py
+++ Driver.py
@@ -10,8 +10,7 @@ from Pattern import *
 from threading import Thread
 import threading
 import util
-import alsaaudio as aa
-import audioop
+from AudioProcessor import AudioProcessor
 
 class Driver:
 	def __init__(self):
@@ -38,7 +37,7 @@ class Driver:
 			sys.exit(3)
 		self.display=self.initDisplay(pattern)
 		
-		self.audioPlaying=False
+		
 		self.audioProcessor=self.initAudio()
 		print("Hello")
 		
@@ -50,53 +49,18 @@ class Driver:
 		return newDisplay;
 		
 	def initAudio(self):
-		#TODO Fix everything here. I did this quick and dirty, not up to documentation
-		#very hacked here
-		# Set up audio
-		print("Begin audio 1")
-		data_in = aa.PCM(aa.PCM_CAPTURE, aa.PCM_NONBLOCK)
-		data_in.setchannels(1)
-		data_in.setrate(44100)
-		data_in.setformat(aa.PCM_FORMAT_S16_LE)
-
-		data_in.setperiodsize(256)
-		
-		
-		
-		return data_in#wrong return
+		newAudio=AudioProcessor(self.display);
+		return newAudio
 	
-	def runAudio(self, data_in):#not set to terminate cleanly
-		audioPattern=Circles()
-		print("audioStarting")
-		while (not self.terminateFlag):
-			# Read data from device
-			l,data = data_in.read()
-			if l:
-				# catch frame error
-				try:
-					max_vol=audioop.max(data,2)#2 Bytes filter below 5000
-					audioPattern.addAmplitudePoint(max_vol)
-					if(max_vol>7000):#be extra sure here to avoid static (formerly 6000)
-						if(not self.audioPlaying):
-							self.audioPlaying=True
-							self.display.currentPattern=audioPattern
-					else:
-						if(self.audioPlaying):
-							self.audioPlaying=False
-				except audioop.error, e:
-					if e.message !="not a whole number of frames":
-						raise e
-			time.sleep(0)#let other threads work
-		print ("Audio Thread Ending")
 		
 	
 	def shutdown(self):
 		self.display.shutdown()
-		#self.audioProcessor.shutdown()
+		self.audioProcessor.shutdown()
 	
 	def start(self):
 		dispThread=Thread(target=self.display.start)
-		audioThread=Thread(target=self.runAudio, args=(self.audioProcessor,))#also wrong input
+		audioThread=Thread(target=self.audioProcessor.start)#also wrong input
 		dispThread.start()
 		audioThread.start()
 		print("Blocking")
