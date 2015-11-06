7c044e5a83d273abd8eab451670d4f80481c90a4
Michael Ripley <zkxs00@gmail.com>
2015-11-05 18:46:24 -0600

    get array

--- AudioProcessor.py
+++ AudioProcessor.py
@@ -10,7 +10,9 @@ from collections import deque
 
 class AudioProcessor:
 	
-	BUFFER_SIZE = 128
+	BUFFER_SIZE = 1024
+	PERIOD_SIZE = 64
+	SAMPLE_BYTES = 2
 	
 	def __init__(self, display):
 		self.terminateFlag=False
@@ -18,7 +20,7 @@ class AudioProcessor:
 		self.data_in.setchannels(1)
 		self.data_in.setrate(44100)
 		self.data_in.setformat(aa.PCM_FORMAT_S16_LE)
-		self.data_in.setperiodsize(64)
+		self.data_in.setperiodsize(self.PERIOD_SIZE)
 		self.audioPlaying=False
 		self.display=display
 		
@@ -34,27 +36,30 @@ class AudioProcessor:
 		while (not self.terminateFlag):
 			# Read data from device
 			l,data = self.data_in.read()
-			sys.stdout.write(data)
-			sys.stdout.write("\xFF\xFF\xFF\xFF\xFF\xFF")
-			if l:
-				# catch frame error
-				try:
-					max_vol=audioop.max(data,4)#2 Bytes filter below 5000 369038784 1417630784
-					#400000000
-
-					#print(max_vol)
-					audioPattern.addAmplitudePoint(max_vol)
-					if(max_vol>util.noiseThreshold):#be extra sure here to avoid static (formerly 6000)
-						if(not self.audioPlaying):
-							self.audioPlaying=True
-							###self.display.currentPattern=audioPattern
-					else:
-						if(self.audioPlaying):
-							self.audioPlaying=False
-				except audioop.error, e:
-					if e.message !="not a whole number of frames":
-						raise e
-			time.sleep(.001)#let other threads work
+			assert l == self.PERIOD_SIZE, "unexpected array size"
+			arr = np.fromstring(data, dtype='<uint16')
+			print(arr)
+			
+			
+			#if l:
+			#	# catch frame error
+			#	try:
+			#		max_vol=audioop.max(data, self.SAMPLE_BYTES) #2 Bytes filter below 5000 369038784 1417630784
+			#		#400000000
+			#		
+			#		#print(max_vol)
+			#		audioPattern.addAmplitudePoint(max_vol)
+			#		if(max_vol>util.noiseThreshold):#be extra sure here to avoid static (formerly 6000)
+			#			if(not self.audioPlaying):
+			#				self.audioPlaying=True
+			#				###self.display.currentPattern=audioPattern
+			#		else:
+			#			if(self.audioPlaying):
+			#				self.audioPlaying=False
+			#	except audioop.error, e:
+			#		if e.message !="not a whole number of frames":
+			#			raise e
+			#time.sleep(.001)#let other threads work
 		print ("Audio Thread Ending")
 	
 	def shutdown(self):
