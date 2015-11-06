04f31f20c6608de6b10765e010f0f7ac2f1e6ad5
Michael Ripley <zkxs00@gmail.com>
2015-11-06 17:26:45 -0600

    Rememebered to include self

--- Pattern.py
+++ Pattern.py
@@ -189,8 +189,8 @@ class Bars(VolumePattern):
 				self.barTopVel[i] += self.barTopAccel
 			self.barTops[i] -= self.barTopVel[i]
 				
-			if (barTops[i] > self.barCaps[i]):
-				self.barCaps[i] = barTops[i]
+			if (self.barTops[i] > self.barCaps[i]):
+				self.barCaps[i] = self.barTops[i]
 				self.barCapVel[i] = 0	
 			else:
 				self.barCapVel[i] += self.barCapAccel
